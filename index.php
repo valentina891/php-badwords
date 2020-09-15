d57db82a24f1aa0f00934cbc8195c6b2d77be9e0
valentina891 <valemil.vs@gmail.com>
2020-09-15 17:46:54 +0200

    end

--- index.php
+++ index.php
@@ -2,21 +2,26 @@
 Visualizzare a schermo il paragrafo con la relativa lunghezza
 e sostituire la badword passata in GET con tre *. -->
 
-<!-- creo variabile con paragrafo testo e lo visualizzo a schermo -->
+
 <?php
 
+// creo variabile con paragrafo testo e lo visualizzo a schermo:
 $testo = '“Quel ramo del lago di Como, che volge a mezzogiorno, tra due catene non interrotte di monti, tutto a seni e a golfi,
 a seconda dello sporgere e del rientrare di quelli, vien quasi a un tratto, tra un promontorio a destra e un’ampia costiera dall’altra parte […]”';
-echo $testo;
+// echo $testo;
 
-// visualizzo la lunghezza
+// dichiaro variabile per la lunghezza:
 $lunghezzaTesto = strlen($testo);
-echo " Questo incipit è lungo $lunghezzaTesto caratteri.";
 
 //dichiaro badword a GET:
-$badword = $_GET[""];
+$badword = $_GET['parola'];
+
 //la sostituisco con tre *:
-$sostituisci = str_replace($badword,'***');
+// var_dump($badword);
+$sostituisci = str_replace($badword,'***', $testo);
 echo $sostituisci;
+
+// visualizzo la lunghezza
+echo " Questo incipit è lungo $lunghezzaTesto caratteri.";
 
 ?>
